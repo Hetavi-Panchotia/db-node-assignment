8194ece25bcef2ab775c7f5cd5ed528da074feba
Hetavi <hetavipanchotia@gmail.com>
2026-04-22 10:50:53 +0530

    feat: add bulk create notes endpoint (POST /api/notes/bulk)

--- A1/src/routes/note.routes.js
+++ A1/src/routes/note.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const NoteRouter = express.Router();
 const {
-    createNote
+    createNote,
+    createNotesBulk
 } = require('../controllers/note.controller');
 
+NoteRouter.post('/bulk', createNotesBulk);
 NoteRouter.post('/', createNote);
 
 module.exports = NoteRouter;
